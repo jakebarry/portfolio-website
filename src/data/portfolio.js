16abdbbc4747a2139fb8166466bd417ba892b34d
jakebarry <168513840+jakebarry@users.noreply.github.com>
2024-08-21 17:24:12 +1000

    first commit

---
+++ src/data/portfolio.js
@@ -0,0 +1,14 @@
+export default [
+    {
+        title: 'Discord Bot: Recipe Generator',
+        imgUrl: '/assets/discord_bot.png',
+        stack: ['Python', 'Discord API', 'Website Scraping'],
+        link:'',
+    },
+    {
+        title: 'Developer Portfolio',
+        imgUrl: '/assets/react_tailwind_vite.jpg',
+        stack: ['React', 'Tailwind', 'Vite'],
+        link:'',
+    },
+]
---
+++ src/data/timeline.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        year: 'Today',
+        title: 'Software Developer / AI Trainer',
+        duration: '3 months',
+        details: 'This is what I did at Data Annotation Tech.',
+    },
+    {
+        year: '2024',
+        title: 'Website Editor / Inventory Manager',
+        duration: '6 months',
+        details: 'B2B and Shopify.',
+    },
+    {
+        year: '2022',
+        title: 'Software Engineer Research Internship',
+        duration: '3 months',
+        details: 'Stemformatics.',
+    },
+    {
+        year: '2020-2022',
+        title: 'Master of Engineering (Mechatronics)',
+        duration: '3 year',
+        details: 'MoE.',
+    },
+    {
+        year: '2016-2018',
+        title: 'Bachelor of Science (Mechatronics)	',
+        duration: '3 years',
+        details: 'BoS.',
+    },
+]
