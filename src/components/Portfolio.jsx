c13c648cc859553f4e92c2cf00b1083fb1bf5078
jakebarry <168513840+jakebarry@users.noreply.github.com>
2024-10-07 16:39:38 +1100

    Added project heading.

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -5,16 +5,19 @@ import PortfolioItem from './PortfolioItem';
 
 function Portfolio() {
     return (
-        <div className='flex flex-col md:flex-row items-center justify-center'>
-            <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4'>
-                {portfolio.map(project => (
-                    <PortfolioItem
-                        imgUrl={project.imgUrl}
-                        title={project.title}
-                        stack={project.stack}
-                        link={project.link}
-                    />
-                ))}
+        <div className=''>
+            <h2 className='text-xl text-center md:text-xl mb-4 font-bold'>Projects</h2>
+            <div className='flex flex-col md:flex-row items-center justify-center'>
+                <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 gap-4'>
+                    {portfolio.map(project => (
+                        <PortfolioItem
+                            imgUrl={project.imgUrl}
+                            title={project.title}
+                            stack={project.stack}
+                            link={project.link}
+                        />
+                    ))}
+                </div>
             </div>
         </div>
     )
